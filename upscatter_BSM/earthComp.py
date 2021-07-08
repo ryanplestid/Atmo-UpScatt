0ad8bdc402fab47cb5f47344ef86b770714c9392
Ryan Plestid <ryanplestid@Ryans-MacBook-Pro.local>
2021-07-08 16:31:40 -0700

    added docstring and module function example to earthComp.py

--- upscatter_BSM/earthComp.py
+++ upscatter_BSM/earthComp.py
@@ -1,7 +1,25 @@
 def main():
-    print("Hello World")
+    module_function("Hello World")
     return 0
 
+def module_function(string):
+    '''
+    Takes a string and prints it to the screen 
+
+    args:
+            string: any string 
+
+    returns:
+            0 
+
+    actions: 
+            prints string to screen    
+    '''
+
+
+    print(string)
+
+    return(0) 
 
 if __name__ == "__main__":
     main()
