a2224a92f5b6bf53ae78081e7bcd5de6f2e6d885
AndrewGustafson <85640582+AndrewGustafson@users.noreply.github.com>
2021-07-22 16:40:26 -0400

    Removed Sampling Function
    Function to Sample and weight energies had been moved to SampleEvents

--- upscatter_BSM/atmoNuIntensity.py
+++ upscatter_BSM/atmoNuIntensity.py
@@ -10,51 +10,6 @@ from scipy import special as spc
 from numpy import random as rand
 from numpy import sin, cos
 
-def Sample_Neutrino_Energies(num_Events, E_min, E_max, power_law):
-    '''
-    This function samples neutrino energies according to a power law
-    
-    args:
-        num_Events: number of energies we wish to sample (int)
-        E_min: minimum Energy of our distribution in GeV (float)
-        E_max: maximum Energy of our distriubtion in GeV (float)
-        power_law: our distibution follows E^{-power_law} (float)
-    
-    returns:
-        Energies: array of length num_Events with the sampled energies (floats)
-    '''
-    kappa = (1 - power_law)/ (E_max**(1-power_law) - E_min**(1-power_law)) #Constant used in sampling
-    rand_chi = rand.rand(num_Events)  #array of random numbers between 0 and 1
-    
-    first_terms = ((1-power_law)/kappa) * rand_chi
-    second_term = E_min**(1-power_law)
-    
-    Energies = (first_terms+second_term)**(1/(1-power_law))
-    
-    return(Energies)
-
-def Calculate_dE(num_Events, Energies, E_min, E_max, power_law):
-    '''
-    This function returns the properly weighted values of dE
-    
-    args:
-        num_Events: number of events in our integral(int)
-        Energies: Neutrino energies of our events (array of floats)
-        E_min: minimum Energy of our distribution in GeV (float)
-        E_max: maximum Energy of our distriubtion in GeV (float)
-        power_law: our distibution follows E^{-power_law} (float)
-    
-    returns:
-        dEs: weighted differentials for calculating the integral
-            (array of floats, same size as Energies)
-    '''
-    num = (1-power_law)*(E_max - E_min)
-    den = E_max**(1-power_law) - E_min**(1-power_law)
-    E_char = (num/den)**(1/power_law) #Characteristic Energy
-    
-    dEs = (E_max - E_min)/(num_Events**(1/3)) * (Energies/E_char)**power_law
-    
-    return(dEs)
 
 def Calc_cos_zeniths(interaction_locs, entry_locs):
     '''
