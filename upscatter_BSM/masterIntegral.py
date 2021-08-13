a33f4c78730a554b123fd1bbd851f16eacecc6c1
AndrewGustafson <85640582+AndrewGustafson@users.noreply.github.com>
2021-08-13 12:09:10 -0400

    Excluded double bangs

--- upscatter_BSM/masterIntegral.py
+++ upscatter_BSM/masterIntegral.py
@@ -37,30 +37,36 @@ power_law = 2 #Guess at energy dependence of atm flux
 flux_name= 'H3a_SIBYLL23C'
 
 R_Earth = 1  # use units where R_earth = 1
-#6378.1 * 1000* 100    #Radius of the Earth (cm)
+R_Earth_cm = 6378.1 * 1000* 100    #Radius of the Earth (cm)
 c = 10 #R_max = c*lambda
-Y = np.array([0,0,R_Earth])     #Location of our detector
-V_det = 300 * (1e6)         #Volume of detector in cm^3
+Y = np.array([0,0,0.9999*R_Earth])     #Location of our detector
+V_det = 32000 * (1e6)         #Volume of detector in cm^3
+
+R_min = (V_det)**(1/3) / R_Earth_cm #minimum distance to detector in R_Earth = 1
+
+A_perp = (V_det)**(2/3) #cm^2
+l_det = (V_det)**(1/3) / R_Earth_cm # units of R_Earth = 1
 
 epsilon = 1e-7 #1 - cos(Theta_min)
 Theta_max = pi
 
 #m_N = 0.01 #HNL mass, GeV
 #d = 1e-9 #Dipole coupling (MeV^-1)
-m_N_vals = np.logspace(-1,-1,1)
-d_vals = np.logspace(-9,-9,1)
+m_N_vals = [0.1]#np.logspace(-2,np.log10(0.3),10) #HNL mass (GeV)
+d_vals = [1e-10]#np.logspace(-10.5,-7,14) #Dipole Coupling (MeV^-1)
 Decays = np.zeros((len(d_vals),len(m_N_vals)))
-num_Events = int(1e5)
+num_Events = int(2.5e5)
 
 alpha_decay = 0 #Used for determining if the HNL is Dirac or Majoranna
 
 #Sample events
 d_index = 0
 for d in d_vals:
-    print(d,'*1e-9 (MeV^-1)')
+    
     m_N_index = 0
     for m_N in m_N_vals:
-        
+        print(d*1e9,'*1e-9 (MeV^-1)')
+        print(m_N, 'GeV')
         #Make a dictionary of the MetaData for the simulation
         Sim_MetaData = {'Flux Choice': flux_name,
                         'Earth Model': 'PREM',
@@ -70,14 +76,14 @@ for d in d_vals:
                         'd (MeV^-1)': d,
                         'm_N (GeV)': m_N,
                         'Detector Location (r_Earth=1)':Y}
+        
         E_min = max(0.1,m_N + .001) #At least 1 MeV above m_N
-        print(m_N, 'GeV')
     
         Energies = SampleEvents.Sample_Neutrino_Energies(num_Events,E_min,E_max,power_law)
         lambdas = dipoleModel.decay_length(d,m_N,Energies)
-        R_maxs = c*lambdas
+        R_maxs = c*lambdas + np.ones(len(lambdas)) * 1.5 * R_min
         
-        X_vect_vals = SampleEvents.Sample_Interaction_Locations(num_Events, Y, R_maxs)
+        X_vect_vals = SampleEvents.Sample_Interaction_Locations(num_Events, Y, R_maxs,R_min)
         Radii = np.sqrt(X_vect_vals[:,0]**2 +X_vect_vals[:,1]**2 + X_vect_vals[:,2]**2) #cm
         rs = Radii/R_Earth #normalized radii
         cos_Thetas = SampleEvents.Sample_cos_Theta(num_Events, epsilon, Theta_max)
@@ -94,7 +100,9 @@ for d in d_vals:
         
         #Prob to decay in detector * Perpendicular Area
         Y_minus_X_mag = np.sqrt((Y[0] - X_vect_vals[:,0])**2 + (Y[1] - X_vect_vals[:,1])**2 + (Y[2] - X_vect_vals[:,2])**2)
-        P_decs_A_Perp = np.exp(-Y_minus_X_mag/lambdas) * V_det/lambdas
+        print(min(Y_minus_X_mag)/l_det, 'min distance/l_det')
+        P_decs_A_Perp = (np.exp(-Y_minus_X_mag/lambdas) *(1 - np.exp(-l_det/lambdas))
+                         * A_perp)
         
         #Calculate weights for events
         E_Range = E_max - E_min
@@ -102,19 +110,19 @@ for d in d_vals:
         Volume = 4*pi/3 * R_Earth ** 3
 
         w_E = SampleEvents.weight_Energy(Energies, E_min, E_max, power_law)
-        w_V = SampleEvents.weight_positions(Y,R_maxs)
+        w_V = SampleEvents.weight_positions(Y,R_maxs, R_min)
         w_Theta = SampleEvents.weight_cos_Theta(cos_Thetas,epsilon, Theta_max)
         tot_weight = (w_E * w_V * w_Theta)
         
-        tot_delta = E_Range * cos_Theta_range * Volume * tot_weight/num_Events
+        tot_delta = R_Earth_cm * E_Range * cos_Theta_range * Volume * tot_weight/num_Events
         
         #Calculate how much each event contributes to the rate
         
         dR = N_d_sigma_d_cos_Thetas * 4 * pi * flux * (P_decs_A_Perp)/(4*pi * Y_minus_X_mag**2) * tot_delta
         Rate = sum(dR)
         Decays[d_index, m_N_index] = Rate
-        print('Number of Events', num_Events)
-        print('Rate', Rate)
+        #print('Number of Events', num_Events)
+        #print('Rate', Rate)
         print('decays in 12 years', Rate * 86400 * 365 *12)
         print('')
         
@@ -123,7 +131,7 @@ for d in d_vals:
         for flavor in flavors:
             flavor_fluxes[flavor] = atmoNuIntensity.calc_fluxes(Energies,cos_zeniths,
                                       flux_name,nu_flavors =[flavor])
-        
+
         #Create a filename to save these events
         filename = "mN_"+str(m_N) +"_d_"+str(d)+".events"
         #Make a list of events
@@ -154,40 +162,40 @@ for d in d_vals:
         with open(filename,'wb') as events_file:
             pickle.dump(Sim_Dictionary, events_file)
         
-        print(Sim_Dictionary['MetaData']['m_N'])
-        
+        #print(Sim_Dictionary['MetaData']['m_N (GeV)'])
+
         m_N_index += 1
     
     d_index += 1
 
 figk, ax = plt.subplots(1,1)
-#levels = np.logspace()
-cp = ax.contourf(m_N_vals, d_vals, Decays * 86400 * 365 * 10, locator = ticker.LogLocator(), cmap = cm.PuBu_r)
+levels = np.logspace(-6,8,8)
+cp = ax.contourf(m_N_vals, d_vals, Decays * 86400 * 365 * 10, levels,locator = ticker.LogLocator(), cmap = cm.PuBu_r)
 figk.colorbar(cp)
 plt.ylabel('d ($MeV^{-1}$)', fontsize = 14)
 plt.yscale('log')
 plt.xlabel('$M_N$ (GeV)', fontsize = 14)
 plt.xscale('log')
 plt.suptitle('Total Decays over 10 years', fontsize = 16)
-plt.title('Monte Carlo Const Dens' + str(num_Events) + ' events', fontsize = 14)
+plt.suptitle('Monte Carlo Total HNL Decays', fontsize = 16)
+plt.title(str(num_Events) + ' events, Super-K Volume, No Double Bang')
 
 
-'''
 #Calculate what is seen by the detector
 cos_zeta_primes = DetectorModule.Calc_cos_zeta_prime(num_Events,alpha_decay)
 zetas, E_gammas = DetectorModule.Calc_Zetas_and_Energies(cos_zeta_primes, Energies, m_N)
 cos_phi_dets = DetectorModule.Calc_cos_phi_det(Y, X_vect_vals, zetas)
 E_midpoints, cos_midpoints, rates = DetectorModule.Rate_In_Each_Bin(0,1.3, 5, 10,E_gammas, cos_phi_dets, dR)
-
 figk, ax = plt.subplots(1,1,figsize = (8,6))
 #levels = np.logspace(-3,3,20)
-cp = ax.contourf(cos_midpoints,E_midpoints,rates*86400*365*12)
-
+cp = ax.contourf(cos_midpoints,E_midpoints,rates*86400*365*10)
 figk.colorbar(cp)
 plt.ylabel('Energy (GeV)')
 plt.xlabel('$\cos(\\theta_z)$')
-plt.title('Total events in each bin for 12 years, $m_N$ ='+str(m_N)+"GeV, d = "+str(d)+"MeV^-1")
-'''  
+plt.ylim([min(E_midpoints),0.7])
+#plt.suptitle('Total events in each bin for 10 years',fontsize = 16)
+#plt.title('$m_N$ ='+str(m_N)+"GeV, d = "+str(d)+"$MeV^{-1}$",fontsize = 14)
+
 
 if __name__ == "__main__":
     main()
